9d2681796b76d71393f444a9c6dcd61e26312dcc
ytghyt <163166198+ytghyt@users.noreply.github.com>
2024-05-27 23:53:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // index.js
+const areObjects = require('./areObjects');
+const areArrays = require('./areArrays');
+const sum = require('./sum');
+
+module.exports = {
+    areObjects,
+    areArrays,
+    sum
+};
